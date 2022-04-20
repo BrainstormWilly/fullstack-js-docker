1aed048c22f01b83c863aa66bef4b454c3d6980c
William Langley <william.langley@hotelengine.com>
2022-04-20 11:23:20 -0700

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
+
+const App = () => (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Fib Calculator</h1>
+        <Link to="/">Home</Link>
+        <Link to="/otherpage">Other Page</Link>
+      </header>
+      <Switch>
+        <Route path="/otherPage" component={OtherPage} />
+        <Route path="/" component={Fib} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+export default App;
